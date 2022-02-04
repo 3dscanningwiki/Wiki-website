5c5840b01c7edf0af1e5b6722f3dcf9a9d543c0d
Tiim <tim.bachmann96@gmail.com>
2022-02-04 15:31:19 +0100

    Make the pages list more accessible

---
+++ src/routes/pages.svelte
@@ -0,0 +1,42 @@
+<script context="module">
+	export async function load({ fetch }) {
+		const content = await fetch(`/api/all_pages.json`).then((r) => r.json());
+		
+		content.sort((a,b) => (a.title??a.slug) > (b.title??b.slug) ? 1: -1)
+		
+		return {
+			props: { content }
+		};
+	}
+</script>
+
+<script>
+import { page } from "$app/stores";
+import A from "./[...slug].svelte";
+
+	export let content;
+</script>
+
+<main class="content">
+<h1>All Wiki Pages</h1>
+<ul>
+	{#each content as page}
+		<li><a href={'/' + page.slug}>{page.title ?? page.slug}</a>
+			{#if page.wip}
+				<span class="tag">WIP</span>
+			{/if}
+		</li>
+	{/each}
+</ul>
+</main>
+
+
+<style>
+	.tag {
+		margin: 0.5rem;
+		font-size: 0.75rem;
+		background-color: var(--accent-1-8);
+		padding: 2px 5px;
+		border-radius: 5px;
+	}
+</style>
